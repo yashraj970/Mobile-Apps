54a86280bc83b10d510a071c47cf8a4c4ad8fb7a
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2024-10-17 17:05:22 +0530

    Create TopTabs.tsx

---
+++ Ionic-app/src/components/TopTabs/TopTabs.tsx
@@ -0,0 +1,22 @@
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+} from "@ionic/react";
+import { Route } from "react-router-dom";
+import {
+  homeOutline,
+  informationCircleOutline,
+  callOutline,
+} from "ionicons/icons";
+import Home from "../../pages/Home/Home";
+import AboutPage from "../../pages/About/About";
+
+const TopTabs = () => {
+  return <></>;
+};
+
+export default TopTabs;
