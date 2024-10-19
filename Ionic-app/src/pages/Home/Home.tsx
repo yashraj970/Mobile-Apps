699657e65e16cc20bd77dd2d60f9d03c030ec7f3
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2024-10-19 22:02:20 +0530

    Update Home.tsx

--- Ionic-app/src/pages/Home/Home.tsx
+++ Ionic-app/src/pages/Home/Home.tsx
@@ -16,32 +16,19 @@ import TopTabs from "../../components/TopTabs/TopTabs";
 
 const Home: React.FC = () => {
   return (
-    <>
-      <TopTabs />
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <Modal />
-        </IonContent>
-        <IonSearchbar placeholder="Placeholder"></IonSearchbar>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 1</IonTitle>
+        </IonToolbar>
+        <TopTabs />
+      </IonHeader>
 
-        <IonItem>
-          <IonSelect
-            label="Fixed label"
-            labelPlacement="fixed"
-            placeholder="Favorite fruit"
-          >
-            <IonSelectOption value="apple">Apple</IonSelectOption>
-            <IonSelectOption value="banana">Banana</IonSelectOption>
-            <IonSelectOption value="orange">Orange</IonSelectOption>
-          </IonSelect>
-        </IonItem>
-      </IonPage>
-    </>
+      <IonContent fullscreen>
+        <Modal />
+      </IonContent>
+      <IonSearchbar placeholder="Placeholder"></IonSearchbar>
+    </IonPage>
   );
 };
 
