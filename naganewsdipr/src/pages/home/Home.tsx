8563b208febef75e439c6891c1db5ce5cca4eca7
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2024-10-20 21:11:42 +0530

    Create Home.tsx

---
+++ naganewsdipr/src/pages/home/Home.tsx
@@ -0,0 +1,26 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonSearchbar,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonSearchbar placeholder="Search" />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
