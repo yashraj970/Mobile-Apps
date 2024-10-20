5462010b400436c5b94d21634e2df6c20d102ea2
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2024-10-20 21:11:52 +0530

    app added

---
+++ naganewsdipr/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "in.gov.nagaland.ipr",
+  appName: "DIPR Nagaland",
+  webDir: "dist",
+};
+
+export default config;
