301cedc01d4e5bd1f4374a019d4d4889352035c8
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2024-10-21 12:20:43 +0530

    Update environment.ts

--- naganewsdipr/src/environments/environment.ts
+++ naganewsdipr/src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  /*   baseurl: 'https://ipr.nagaland.gov.in/',
+    baseurl: 'https://ipr.nagaland.gov.in/',
     placeholderimage: 'assets/imgs/ipr.jpg',
-    pdfBaseURl: 'https://ipr.nagaland.gov.in' */
-  baseurl: 'https://ipr.ramietech.in/',
-  placeholderimage: 'assets/imgs/ipr.jpg',
-  pdfBaseURl: 'https://ipr.ramietech.in',
+    pdfBaseURl: 'https://ipr.nagaland.gov.in',
+  // baseurl: 'https://ipr.ramietech.in/',
+  // placeholderimage: 'assets/imgs/ipr.jpg',
+  // pdfBaseURl: 'https://ipr.ramietech.in',
   version: 11,
   shareUrl: 'https://dipr-53782.web.app',
 };
