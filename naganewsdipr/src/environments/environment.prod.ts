10c21c53cde3ab2537ff9a65550f6ae4e2ef1f6f
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2024-10-21 11:59:36 +0530

    Create environment.prod.ts

---
+++ naganewsdipr/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  /*   baseurl:'https://ipr.nagaland.gov.in/',
+    placeholderimage:'assets/imgs/ipr.jpg',
+    pdfBaseURl:'https://ipr.nagaland.gov.in' */
+  baseurl: 'https://ipr.ramietech.in/',
+  placeholderimage: 'assets/imgs/ipr.jpg',
+  pdfBaseURl: 'https://ipr.ramietech.in',
+  version: 11,
+  shareUrl: 'https://dipr-53782.web.app',
+
+ 
+};
