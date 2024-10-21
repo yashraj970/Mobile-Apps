071c9bd9d144df2a7e9732a124decdc3fc9d1fb4
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2024-10-21 11:59:38 +0530

    Create environment.ts

---
+++ naganewsdipr/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  /*   baseurl: 'https://ipr.nagaland.gov.in/',
+    placeholderimage: 'assets/imgs/ipr.jpg',
+    pdfBaseURl: 'https://ipr.nagaland.gov.in' */
+  baseurl: 'https://ipr.ramietech.in/',
+  placeholderimage: 'assets/imgs/ipr.jpg',
+  pdfBaseURl: 'https://ipr.ramietech.in',
+  version: 11,
+  shareUrl: 'https://dipr-53782.web.app',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
