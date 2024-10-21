56d0b5dfbb9dfdda66d9742955e1f301700a41cc
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2024-10-21 13:02:20 +0530

    Update Home.tsx

--- naganewsdipr/src/pages/home/Home.tsx
+++ naganewsdipr/src/pages/home/Home.tsx
@@ -7,6 +7,9 @@ import {
   IonToolbar,
 } from "@ionic/react";
 import React from "react";
+import HomeNewsList from "./home-news-list/home-news-list";
+import HomePhotoNews from "./home-news/HomePhotoNews";
+import HomeVideoNews from "./home-news/HomeVideoNews";
 
 const Home: React.FC = () => {
   return (
@@ -16,10 +19,12 @@ const Home: React.FC = () => {
           <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
-      
-      <IonContent>
-        <IonSearchbar placeholder="Search" />
-      </IonContent>
+
+      <IonSearchbar placeholder="Search" />
+
+      {/* <HomeNewsList newstype={"photonews"} /> */}
+      {/* <HomePhotoNews /> */}
+      {/* <HomeVideoNews /> */}
     </IonPage>
   );
 };
